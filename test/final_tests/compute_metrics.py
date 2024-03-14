775e8472cd8a5832b8f1c6cecf361c6fc0b18968
Shreya Reddy Terupally <shreyaterupallyiiit@gmail.com>
2024-03-13 20:11:04 -0400

    faculty metrics

--- test/final_tests/compute_metrics.py
+++ test/final_tests/compute_metrics.py
@@ -24,7 +24,7 @@ def map_at_k(retrieved, ground_truth, k=1):
         if len(true_docs) == 0:  # Handle case with no relevant documents
             continue
         average_precisions.append(average_precision_score(y_true, [1] * len(y_true)))
-    return 100.0 * np.mean(average_precisions) if average_precisions else 0
+    return 100.0 * np.mean(average_precisions) if len(average_precisions) else 0
 
 
 #Answer - semantic similarity
@@ -33,14 +33,13 @@ def compute_answer_BERT_similarity(question, answers, predicted_answers):
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     model = BertModel.from_pretrained('bert-base-uncased')
 
-    print(len(question), len(answers), len(predicted_answers))
-    print(question[-1], answers[-1], predicted_answers[-1])
     # Storage for cosine similarities
     similarities = []
 
     for i in range(len(predicted_answers)):
         # Format the ground truth and predicted strings
-        true_ans, pred_ans = answers[i].strip().strip('\n'), predicted_answers[i].strip().strip('\n')
+        true_ans, pred_ans = str(answers[i]).strip().strip('\n'), str(predicted_answers[i]).strip().strip('\n')
+        #print(question[i], true_ans, pred_ans)
         ground_truth = f"Q: {question[i]}\nA: {true_ans}"
         predicted = f"Q: {question[i]}\nA: {pred_ans}"
 
@@ -64,30 +63,36 @@ def compute_answer_BERT_similarity(question, answers, predicted_answers):
 
 #Answer - exact match
 def answer_exact_match(true_answers, pred_answers):
-    match_count = sum(1 for pred, true in zip(pred_answers, true_answers) if pred.strip().lower() == true.strip().lower())
-    return 100.0* match_count / len(true_answers) if true_answers else 0
+    match_count = sum(1 for pred, true in zip(pred_answers, true_answers) if str(pred).strip().lower() == str(true).strip().lower())
+    return 100.0* match_count / len(true_answers) if len(true_answers) else 0
 
 #Answer recall - No. of overlapping words/ no.of words in gt answer
 def answer_recall(true_answers, pred_answers):
     recalls = []
     for pred, true in zip(pred_answers, true_answers):
-        pred_words = Counter(pred.lower().split())
-        true_words = Counter(true.lower().split())
+        pred_words = Counter(str(pred).lower().split())
+        true_words = Counter(str(true).lower().split())
         common_words = pred_words & true_words
         total_true_words = sum(true_words.values())
         recall = sum(common_words.values()) / total_true_words if total_true_words > 0 else 0
         recalls.append(recall)
     return 100.0* np.mean(recalls)
 
-if __name__ == '__main__':
+def listify(str_list_full):
+    full = []
+    for str_list in str_list_full:
+        ls = str_list.strip()[1:-1].split(",")
+        full.append([p.strip() for p in ls])
+    return full
 
-    results_path = 'Colbert_llama_pred_answers@2.csv'
+if __name__ == '__main__':
+    results_path = 'Colbert_llama_pred_answers@1.csv'
     df = pd.read_csv(results_path)
-    questions = df['question']
-    retrieved_docs = df['retrieved_docs']
-    gt_docs = df['gt_docs']
-    pred_answer = df['llm_answer']
-    gt_answer = df['gt_answer']
+    questions = df['question'].tolist()
+    retrieved_docs = listify(df['retrieved_docs'].tolist())
+    gt_docs = listify(df['gt_docs'].tolist())
+    pred_answer = df['llm_answer'].tolist()
+    gt_answer = df['gt_answer'].tolist()
 
     print(f"Metrics for results : {results_path}")
     print(f"Retrieval Metrics")
@@ -101,6 +106,6 @@ if __name__ == '__main__':
     print("Answer match metrics")
     EM_prec = answer_exact_match(gt_answer, pred_answer)
     ans_recall = answer_recall(gt_answer, pred_answer)
-    ans_sem_prec = compute_answer_BERT_similarity(questions, gt_answers, pred_answers)
+    ans_sem_prec = compute_answer_BERT_similarity(questions, gt_answer, pred_answer)
     print(f"Exact match prec:{EM_prec}\nAnswer recall:{ans_recall}\nSemantic similarity:{ans_sem_prec}") 
 
